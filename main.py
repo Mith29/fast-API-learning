80c1651783f3d757b91c47819d76795eb8031ab5
Mithra <mithra29.venu@gmail.com>
2026-04-22 13:42:20 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"message": "This is my first API!"}
+
+@app.get("/name")
+def read_root1():
+    return {"message": "This is mithra!"}
